bfa996ed230056774f1c885a11ab9c6d445d38e4
Ramanna <ramannaagre@gmail.com>
2026-02-13 00:48:28 +0530

    addeed slice tests and unit tests for controller

---
+++ src/test/java/com/example/order/management/slice/OrderControllerSliceTest.java
@@ -0,0 +1,60 @@
+package com.example.order.management.slice;
+
+import com.example.order.management.controller.OrderController;
+import com.example.order.management.dto.OrderRequest;
+import com.example.order.management.dto.OrderResponse;
+import com.example.order.management.enums.OrderStatus;
+import com.example.order.management.service.OrderService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(OrderController.class)
+@AutoConfigureMockMvc
+public class OrderControllerSliceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private OrderService service;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testCreateOrder() throws Exception {
+
+        OrderRequest request = new OrderRequest();
+        request.setCustomerId("110");
+        request.setAmount(2500);
+
+        OrderResponse response = new OrderResponse();
+        response.setId(1L);
+        response.setCustomerId("110");
+        response.setAmount(1000);
+        response.setStatus(OrderStatus.CREATED);
+
+        Mockito.when(service.createOrder(request)).thenReturn(response);
+
+        mockMvc.perform(post("/orders")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.customerId").value("110"))
+                .andExpect(jsonPath("$.amount").value(1000))
+                .andExpect(jsonPath("$.status").value("CREATED"));
+
+    }
+}
--- src/test/java/com/example/order/management/unit/OrderControllerTest.java
+++ src/test/java/com/example/order/management/unit/OrderControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.order.management.unit;
 
+import com.example.order.management.controller.OrderController;
 import com.example.order.management.dto.OrderRequest;
 import com.example.order.management.dto.OrderResponse;
 import com.example.order.management.entity.Order;
@@ -12,20 +13,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderControllerTest {
 
-
-    @InjectMocks
+    @Mock
     OrderService service;
 
-    @Mock
-    OrderRepository repository;
+    @InjectMocks
+    OrderController controller;
 
     @Test
-    void testCreateOrder(OrderRequest request){
+    void testCreateOrder(){
         OrderRequest order = new OrderRequest();
         order.setCustomerId("21");
         order.setAmount(1000);
@@ -35,8 +36,14 @@ public class OrderControllerTest {
         response.setAmount(1000);
         response.setStatus(OrderStatus.CREATED);
 
-        when(service.createOrder(request)).thenReturn(response);
+        when(service.createOrder(order)).thenReturn(response);
 
-        OrderResponse response2 = service.createOrder(request);
+        OrderResponse testResponse = controller.createOrder(order);
+
+        assertNotNull(testResponse);
+
+        assertEquals(response.getCustomerId(), testResponse.getCustomerId());
+        assertEquals(response.getAmount(), testResponse.getAmount());
+        assertEquals(response.getStatus(), testResponse.getStatus());
     }
 }
