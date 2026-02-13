6acd6c8797b14cc9a02fc601fb74e3c15f9fba42
Ramanna <ramannaagre@gmail.com>
2026-02-13 23:41:50 +0530

    more slice tests + db tests

--- src/test/java/com/example/order/management/slice/OrderControllerSliceTest.java
+++ src/test/java/com/example/order/management/slice/OrderControllerSliceTest.java
@@ -6,6 +6,7 @@ import com.example.order.management.dto.OrderResponse;
 import com.example.order.management.enums.OrderStatus;
 import com.example.order.management.service.OrderService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
@@ -15,7 +16,10 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
 
+import static com.example.order.management.enums.OrderStatus.CREATED;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -43,9 +47,9 @@ public class OrderControllerSliceTest {
         response.setId(1L);
         response.setCustomerId("110");
         response.setAmount(1000);
-        response.setStatus(OrderStatus.CREATED);
+        response.setStatus(CREATED);
 
-        Mockito.when(service.createOrder(request)).thenReturn(response);
+        when(service.createOrder(request)).thenReturn(response);
 
         mockMvc.perform(post("/orders")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -55,6 +59,26 @@ public class OrderControllerSliceTest {
                 .andExpect(jsonPath("$.customerId").value("110"))
                 .andExpect(jsonPath("$.amount").value(1000))
                 .andExpect(jsonPath("$.status").value("CREATED"));
+    }
 
+    @Test
+    void testGetOrder() throws Exception {
+
+        Long orderId = 2L;
+
+        OrderResponse response = new OrderResponse();
+        response.setId(2L);
+        response.setCustomerId("305");
+        response.setAmount(1500);
+        response.setStatus(CREATED);
+
+        Mockito.when(service.getOrder(2L)).thenReturn(response);
+
+        mockMvc.perform(get("/orders/{id}", orderId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(2))
+                .andExpect(jsonPath("$.customerId").value("305"))
+                .andExpect(jsonPath("$.amount").value(1500))
+                .andExpect(jsonPath("$.status").value("CREATED"));
     }
 }
---
+++ src/test/java/com/example/order/management/slice/OrderRepositorySliceTest.java
@@ -0,0 +1,11 @@
+package com.example.order.management.slice;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
+public class OrderRepositorySliceTest {
+
+}
